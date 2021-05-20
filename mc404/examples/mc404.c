cf6662e33aae049167fa217de87d49c9ba5057d5
tr0fin0 <guitrofino@gmail.com>
2021-05-20 14:08:50 -0300

    :construction: add triangule test code

--- mc404/examples/mc404.c
+++ mc404/examples/mc404.c
@@ -2,14 +2,19 @@
 
 int main()
 {
-    int t0 = 0;
-    int t1 = 100;
-    int t2 = 0;
+    int s0 = 3;
+    int s1 = 4;
+    int s2 = 5;
+    int a0 = 1;
 
-    for (t0 = 0; t0 < t1; t0++)
+    if(
+        (s0+s1-s2) <= 0 ||
+        (s1+s2-s0) <= 0 ||
+        (s2+s0-s1) <= 0
+    )
     {
-        t2 += t0;
+        a0 = 0;
     }
 
-    printf("t2 = %d\n", t2);
+    printf("a0 = %d\n", a0);
 }
