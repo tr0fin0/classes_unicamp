2e2a16d2e0de65692af917cf5239e161de91f2f9
tr0fin0 <guitrofino@gmail.com>
2021-05-20 14:08:12 -0300

    :construction: add triangule test code

--- mc404/examples/mc404.s
+++ mc404/examples/mc404.s
@@ -1,14 +1,34 @@
-    main:
-        addi t0, zero, 9
-        addi t1, zero, 0
-        addi t2, zero, 5
+// posible triangule code
+// s0 side A of the triangule
+// s1 side B of the triangule
+// s2 side C of the triangule
+//
+// if a0 == 0 impossible triangule
+//       == 1   possible triangule
+main:
+    addi s0, zero, 3
+    addi s1, zero, 4
+    addi s2, zero, 6
 
-        bne t0, t2, else
-        addi t1, t1, 7
-        j fim
+    add s3, s0, s1
+    sub s3, s3, s2
+    bge zero, s3, else
 
-    else:
-        addi t1, t1, 15
+    add s3, s1, s2
+    sub s3, s3, s0
+    bge zero, s3, else
 
-    fim:
-        jr ra
+    add s3, s2, s0
+    sub s3, s3, s2
+    bge zero, s3, else
+
+    sub a0, a0, a0
+    addi a0, zero, 1
+    j fim
+
+else:
+    sub a0, a0, a0
+    add a0, zero, zero
+
+fim:
+    jr ra
