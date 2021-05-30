d176e415607e32b0e7319dce7657cb6c26bcf5da
tr0fin0 <guitrofino@gmail.com>
2021-05-30 19:52:34 -0300

    :construction: edit test code

--- mc404/examples/mc404_test.s
+++ mc404/examples/mc404_test.s
@@ -1,29 +1,33 @@
-// test can be done in the following site:
-// https://ascslab.org/research/briscv/simulator/simulator.html#
+multiply:
+    add t0, zero, zero # t0 = zero + zero
+
+    deslocation:
+        # addi t1, a1, 1 # t1 = a1 + 1
+        # srai a1, a1, 1 # right shift of a1 by 1
+        addi t1, a1, 0 # t1 = a1 + 1, store a1
+
+        # remove LSB
+        srai a1, a1, 1 # right shift of a1 by 1
+        slli a1, a1, 1 #  left shift of a1 by 1
+
+        # get LSB
+        sub t1, t1, a1 # t1 = t1 - a1
+        srai a1, a1, 1 # right shift of a1 by 1
+
+        beq t1, zero, pass # if t1 == zero then pass
+        add t0, t0, a0     # t0 = t0 + a0
+
+    pass:
+        slli a0, a0, 1             # left shift of a0 by 1
+        bne  a1, zero, deslocation # if a1 != zero then deslocation
+        add  a0, t0, zero          # a0 = t0 + zero
+        jr ra                      # return
+
 main:
-    addi s0, zero, 3
-    addi s1, zero, 4
-    addi s2, zero, 7
 
-    add s3, s0, s1
-    sub s3, s3, s2
-    bge zero, s3, else
+    addi a0, zero, 9
+    addi a1, zero, 9
 
-    add s3, s1, s2
-    sub s3, s3, s0
-    bge zero, s3, else
+    jal multiply
 
-    add s3, s2, s0
-    sub s3, s3, s2
-    bge zero, s3, else
-
-    sub a0, a0, a0
-    addi a0, zero, 1
-    j fim
-
-else:
-    sub a0, a0, a0
-    add a0, zero, zero
-
-fim:
     jr ra
