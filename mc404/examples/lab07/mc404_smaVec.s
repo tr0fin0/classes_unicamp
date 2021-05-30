baa92ec8ea29ba9c1b92e5b01974879444e6002c
tr0fin0 <guitrofino@gmail.com>
2021-05-30 12:13:37 -0300

    :sparkles: add lab07 folder

---
+++ mc404/examples/lab07/mc404_smaVec.s
@@ -0,0 +1,7 @@
+// -----------------------
+// smaller value in vector
+// -----------------------
+// 
+// menorVetor(int *v, int n)
+// int *v, vector with n int's
+// int n,  size of vector's
---
+++ mc404/examples/lab07/mc404_sumVec.s
@@ -0,0 +1,9 @@
+// --------------
+// sum of vectors
+// --------------
+// 
+// somaVetor(int *a, int *b, int *c, int n)
+// int *a, stores the result of *b + *c
+// int *b, vector with n int's
+// int *c, vector with n int's
+// int n,  size of vector's
