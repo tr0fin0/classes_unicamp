cd644c624f673134d1096b963477b04d9c46f12c
tr0fin0 <guitrofino@gmail.com>
2021-05-30 12:16:35 -0300

    :construction: edit comment

--- mc404/examples/lab06/mc404_mul.s
+++ mc404/examples/lab06/mc404_mul.s
@@ -1,3 +1,9 @@
 // -------------------------------
 // multiplication with deslocation
 // -------------------------------
+//
+// s0 integer number >= 0
+// s1 integer number >= 0
+// a0 result of s0*s1
+//
+// operation done without multiplication instruction
