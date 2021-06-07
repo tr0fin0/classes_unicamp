65db5a88393ea8f6ee5eadc673f7f11c569e59fc
tr0fin0 <guitrofino@gmail.com>
2021-06-07 09:04:13 -0300

    :bulb: code operation

--- mc404/examples/lab07/mc404_sumVec.s
+++ mc404/examples/lab07/mc404_sumVec.s
@@ -3,46 +3,13 @@
 # --------------
 # 
 # somaVetor(int *a, int *b, int *c, int n)
-# int *a, stores the result of *b + *c
-# int *b, vector with n int's
-# int *c, vector with n int's
+# int *a, pointer to vector with n int's
+# int *b, pointer to vector with n int's
+# int *c, pointer to vector with n int's
 # int n,  size of vector's
 
-# # declare values
-# addi s0, zero, 1;
-# addi s1, zero, 2;
-# addi s2, zero, 3;
-# addi s3, zero, 4;
-# addi s4, zero, 5;
+# a[i] = b[i] + c[i]
 
-# # declare stack and store values
-# addi sp, sp, -20;
-# sw s0, 0(sp)
-# sw s1, 4(sp)
-# sw s2, 8(sp)
-# sw s3, 12(sp)
-# sw s4, 16(sp)
-# addi sp, sp, -20;
-# sw s0, 0(sp)
-# sw s1, 4(sp)
-# sw s2, 8(sp)
-# sw s3, 12(sp)
-# sw s4, 16(sp)
-
-# # declare a1 = *c
-# addi a0, sp, -20
-# addi a1, sp,   0
-# addi a2, sp,  20
-# addi a3, zero, 5
-
-# jal SomaVetor
-
-# # remove stack and retrive values
-# lw s4, 16(sp)
-# lw s3, 12(sp)
-# lw s2, 8(sp)
-# lw s1, 4(sp)
-# lw s0, 0(sp)
 
 SomaVetor:
     # inicialize
@@ -68,12 +35,12 @@ SomaVetor:
         add t0, t1, t2 # store total value
         sw  t0, 0(s0)  # A[j] = B[j] + C[j]
 
-        addi s0, s0, 4 # *A[j] -> A[j+1]
-        addi s1, s1, 4 # *B[j] -> B[j+1]
-        addi s2, s2, 4 # *C[j] -> C[j+1]
+        addi s0, s0, 4 # *A[j] = A[j+1]
+        addi s1, s1, 4 # *B[j] = B[j+1]
+        addi s2, s2, 4 # *C[j] = C[j+1]
 
         addi t3, t3, 1 # i = i + 1
 
-        blt t3, s3, loop # if i < n then loop
+        blt t3, s3, loop # if i lt n then loop
 
     jr ra # return
