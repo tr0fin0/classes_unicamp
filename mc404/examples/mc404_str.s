1fe082bf89182cd796de45d4d745159dfd42d727
tr0fin0 <guitrofino@gmail.com>
2021-06-10 10:40:28 -0300

    :sparkles: add strcmp function

--- mc404/examples/mc404_str.s
+++ mc404/examples/mc404_str.s
@@ -13,7 +13,7 @@ strlen:
         addi a0, t0, 0 # a0 = i
 ret
 
-# char *strcpy( char *destination, const char *source)
+# char *strcpy(char *destination, const char *source)
 strcpy:
     add t0, a0, zero # t0 = *destination
 
@@ -22,8 +22,25 @@ strcpy:
         sbu t1, 0(a0)       # a0 = a1
         addi a0, a0, 1      # a0++
         addi a1, a1, 1      # a1++
-        bne t1, zero, loop; # if schar != \0 then end
+        bne t1, zero, loop # if schar != \0 then end
 
     add a0, t0, zero # a0 = *destination
+
+ret
+
+# int strcmp(const char *str1, const char *str2)
+strcmp:
+    addi a0, zero, 1 # a0 = 1, str1 == str2
+
+    loop:
+        lbu t0, 0(a0)       # t0 = str1[i]
+        lbu t1, 0(a1)       # t1 = str2[i]
+        addi a0, a0, 1      # str1++
+        addi a1, a1, 1      # str2++
+        bne t0, t1, end    # if str1[i] != str2[i] then end
+        bne t0, zero, loop # if str1[i] != \0      then loop
+
+    end:
+        addi a0, zero, 0 # a0 = 0, str1 != str2
 
 ret
