727233128a8138ed525c3a6e9e1218c751bccb0f
tr0fin0 <guitrofino@gmail.com>
2021-06-17 19:40:11 -0300

    :construction: edit test

--- mc404/examples/lab08/mc404_upperCase.s
+++ mc404/examples/lab08/mc404_upperCase.s
@@ -8,27 +8,32 @@
 # receives a string ended in \0
 # and return the same string with
 # all it is letters upper case
+# 
+#     .TEXT:
+#         .word 0x61605E5C
+#         .word 0x796E6462
+#         .word 0x00007B7A
 
 Maiuscula:
-    addi t0, a0, 0     # save *string
-    addi t2, zero,  97 #a in ASCII
-    addi t3, zero, 122 #z in ASCII
+    addi t0, a0, 0        # save *string
+    addi t2, zero,  97    # a in ASCII
+    addi t3, zero, 122    # z in ASCII
 
-    loop:
-        lbu t1, 0(a0)     #    char = string[i]
-        beq t1, zero, end # if char == \0 then end
+    loop1:
+        lbu t1, 0(a0)      #    char = string[i]
+        beq t1, zero, end1 # if char == \0 then end
 
-        addi a0, a0, 4    # a0++
+        addi a0, a0, 1     # a0++
 
-        blt t1, t2, loop  # if char  a then loop
-        bgt t1, t3, loop  # if char  z then loop
+        blt t1, t2, loop1  # if char  a then loop
+        bgt t1, t3, loop1  # if char  z then loop
 
-        lbu  t1,  -4(a0)  #      char = string[i]
-        addi t1, t1, -32  #       "A" = "a" - 32
-        sb   t1,  -4(a0)  # string[i] = CHAR
+        lbu  t1,  -1(a0)   #      char = string[i]
+        addi t1, t1, -32   #       "A" = "a" - 32
+        sb   t1,  -1(a0)   # string[i] = CHAR
 
-        j loop
+        j loop1
 
-    end:
-        addi a0, t0, 0 # return *string
+    end1:
+        addi a0, t0, 0     # return *string
         jr ra
