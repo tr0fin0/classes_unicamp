3d0ce47ffa47d224e5c5bacd09111cb0aab64b20
tr0fin0 <guitrofino@gmail.com>
2021-06-21 18:09:20 -0300

    :bulb: edit and aligin comments

--- mc404/examples/lab08/mc404_upperCase.s
+++ mc404/examples/lab08/mc404_upperCase.s
@@ -15,25 +15,25 @@
 #         .word 0x00007B7A
 
 Maiuscula:
-    addi t0, a0, 0        # save *string
-    addi t2, zero,  97    # a in ASCII
-    addi t3, zero, 122    # z in ASCII
+    addi t0, a0, 0          # save *string
+    addi t2, zero,  97      # a in ASCII
+    addi t3, zero, 122      # z in ASCII
 
-    loop1:
-        lbu t1, 0(a0)      #    char = string[i]
-        beq t1, zero, end1 # if char == \0 then end
+    loop:
+        lbu t1, 0(a0)       #    char = string[i]
+        beq t1, zero, end   # if char == \0 then end
 
-        addi a0, a0, 1     # a0++
+        addi a0, a0, 1      # a0++
 
-        blt t1, t2, loop1  # if char  a then loop
-        bgt t1, t3, loop1  # if char  z then loop
+        blt t1, t2, loop    # if char  a then loop
+        bgt t1, t3, loop    # if char  z then loop
 
-        lbu  t1,  -1(a0)   #      char = string[i]
-        addi t1, t1, -32   #       "A" = "a" - 32
-        sb   t1,  -1(a0)   # string[i] = CHAR
+        lbu  t1,  -1(a0)    #      char = string[i]
+        addi t1, t1, -32    #       "A" = "a" - 32
+        sb   t1,  -1(a0)    # string[i] = CHAR
 
-        j loop1
+        j loop
 
-    end1:
-        addi a0, t0, 0     # return *string
+    end:
+        addi a0, t0, 0      # return *string
         jr ra
