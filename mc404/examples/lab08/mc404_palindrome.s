58b9af56a651fdcdfee6f46a9d0dbe04c2da1220
tr0fin0 <guitrofino@gmail.com>
2021-06-23 08:56:21 -0300

    :sparkles: add Maiuscula function

--- mc404/examples/lab08/mc404_palindrome.s
+++ mc404/examples/lab08/mc404_palindrome.s
@@ -17,6 +17,30 @@
 #         .word 0x796E6462
 #         .word 0x00007B7A
 
+Maiuscula:
+    addi t0, a0, 0          # save *string
+    addi t2, zero,  97      # a in ASCII
+    addi t3, zero, 122      # z in ASCII
+
+    loop:
+        lbu t1, 0(a0)       #    char = string[i]
+        beq t1, zero, end   # if char == \0 then end
+
+        addi a0, a0, 1      # a0++
+
+        blt t1, t2, loop    # if char  a then loop
+        bgt t1, t3, loop    # if char  z then loop
+
+        lbu  t1,  -1(a0)    #      char = string[i]
+        addi t1, t1, -32    #       "A" = "a" - 32
+        sb   t1,  -1(a0)    # string[i] = CHAR
+
+        j loop
+
+    end:
+        addi a0, t0, 0      # return *string
+        jr ra
+
 Palindrome:
     addi sp, sp, -4
     sw   ra, 0(sp)
