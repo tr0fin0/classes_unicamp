b2bde8a05bd64a22b681670964dc20a606992f89
tr0fin0 <guitrofino@gmail.com>
2022-04-12 13:47:02 -0300

    ✨ add main functions

--- em607/forcedDampingVibration.py
+++ em607/forcedDampingVibration.py
@@ -4,7 +4,19 @@ import matplotlib
 
 def main():
 
-    print("main")
+    r    = np.linspace(0, 5, 10000)
+
+    zetaRef     = np.sqrt(2)/2
+    zetaValues  = [0, 0.15, 0.2, 0.5, zetaRef, 1]
+    # zetaValues  = np.linspace(0, 1, 8)
+
+    if True:
+    # if False:
+        MFvalues(r, zetaRef, zetaValues)
+
+    # if True:
+    if False:
+        thetaValues(r, zetaValues)
 
     return
 
