770e578dde1a50c4216257efa9b40de236c1459a
tr0fin0 <guitrofino@gmail.com>
2022-06-17 10:37:56 -0300

    ✨ add asymptones

--- em607/receptivity.m
+++ em607/receptivity.m
@@ -1,13 +1,14 @@
-function r = receptivity(m, c, k)
+function [w, wL, wH, rcp, asymL, asymH] = receptivity(step, m, c, k)
     % receptivity - This function computes the receptivity of 1DOF Forced 
     % Vibration Vibration System with Viscous Damping
-    %
-    % Syntax: r = receptivity(m, c, k)
-    %
-    % Long description
-    % Receptivity is the ratio of the Displacement over the Force Module
 
-    syms omega
-    r = 1 / (k - omega^2 * m + 1i*omega*c);
 
+    wn = sqrt(k/m);                             % Natural Frequency
+    w  = wn*10^(-2) : step : wn*10^(+2);        % Total Frequency Range
+    wL = wn*10^(-2) : step : wn;                % Low   Frequency Range
+    wH =         wn : step : wn*10^(+2);        % High  Frequency Range
+
+    rcp   = 1 ./ (k - w.^2.* m + w.* (1i*c));   % Receptivity
+    asymL = ( 1/k * ones(size(wL)) );           % Low  Frequency Asymptote
+    asymH = ( 1./(wH.^2 .* m) );                % High Frequency Asymptote
 end
