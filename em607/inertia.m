637a520e4eee9ff3ece877e9b21491653f8fe056
tr0fin0 <guitrofino@gmail.com>
2022-06-17 10:37:46 -0300

    ✨ add asymptones

--- em607/inertia.m
+++ em607/inertia.m
@@ -1,13 +1,11 @@
-function i = inertia(m, c, k)
-    %inertia - This function computes the mobility of 1DOF Forced 
+function [w, wL, wH, inr, asymL, asymH] = inertia(step, m, c, k)
+    % inertia - This function computes the mobility of 1DOF Forced 
     % Vibration Vibration System with Viscous Damping
-    %
-    % Syntax: i = Inertia(m, c, k)
-    %
-    % Long description
-    % Inertia is the ratio of the Acceleration over the Force Module
 
-    syms omega
-    i = 1i * omega * mobility(m, c, k);
-    % i = (-omega^2) / (k - omega^2 * m + 1i*omega*c);
+
+    [w, wL, wH, mbl, asymL, asymH] = mobility(step, M, C, K);
+
+    inr   = 1i .* w .* mbl;                 % Inertia
+    asymL = ( (-wL.^2) ./ k );              % Low  Frequency Asymptote
+    asymH = ( 1/m * ones(size(wH)) );       % High Frequency Asymptote
 end
