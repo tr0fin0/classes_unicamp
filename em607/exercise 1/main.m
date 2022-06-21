8bf39abc5bb78cf36c98cba39acdebb492c29f66
tr0fin0 <guitrofino@gmail.com>
2022-06-21 13:47:14 -0300

    📝 add comments

--- em607/exercise 1/main.m
+++ em607/exercise 1/main.m
@@ -8,6 +8,7 @@ close all
 RA = 217276;
 feature('DefaultCharacterSet','UTF-8')
 
+%   Frequency Analysis
 %   1DOF Forced Vibration Vibration System with Viscous Damping
 %   mddx + cdx + kx = f e^(iwt)
 m   = 1;    % mass
@@ -25,13 +26,13 @@ phi = c / (2 * wn * m); % damping coeficient
 % %%                    Exercices
 % %%============================================================================
 
-ex2(step, 1, 1, 1);
-ex3(step, 1, 1, 1);
-ex4(step, 1, 1, 1);
-ex5(step, 1, 1, 1);
-ex6(step, 1, 5, 100);
-ex7(step, 10, 5, 100);
-ex8(step, 1, 1, 1);
-ex9(step, 1, 1, 1);
-ex10(step, 1, 1, 1);
-ex15(1, 1, 1);
+% ex2(step, 1, 1, 1);
+% ex3(step, 1, 1, 1);
+% ex4(step, 1, 1, 1);
+% ex5(step, 1, 1, 1);
+% ex6(step, 1, 5, 100);
+% ex7(step, 10, 5, 100);
+% ex8(step, 1, 1, 1);
+% ex9(step, 1, 1, 1);
+% ex10(step, 1, 1, 1);
+% ex15(1, 1, 1);
